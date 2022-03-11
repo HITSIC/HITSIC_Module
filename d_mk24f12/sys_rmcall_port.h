dca4731c8ca2892e025a7a00924e40609d1e0318
王沛 <1547963558@qq.com>
2022-03-12 00:16:06 +0800

    端口修复

--- d_mk24f12/sys_rmcall_port.h
+++ d_mk24f12/sys_rmcall_port.h
@@ -14,26 +14,10 @@ extern uart_handle_t UART_BLE_handle;
 extern uart_transfer_t UART_BLE_rxTransfer;
 extern uart_transfer_t UART_BLE_txTransfer;
 
-#define HITSIC_RMCALL_HEADER_MAGIC 0x554768A0U
+#define RMCALL_HEADER_MAGIC 0x554768A0U
 #define RMCALL_SYSLOG_LVL (3U)
-#define HITSIC_RMCALL_PUBLIC_BUF_SIZE (256U)
+#define RMCALL_PUBLIC_BUF_SIZE (256U)
 #define kStatusGroup_RMCALL (201U)
-
-//数据包定义
-__PACKED struct _ECG_Data_Packet_t
-{
-	uint32_t waveform;
-	uint32_t func;
-	uint32_t lead;
-	float voltage;
-	float period;
-};
-typedef struct _ECG_Data_Packet_t ECG_Data_Packet_t;
-//指令宏
-#define GEN_STATUS_ID    0xaaaa
-#define GEN_HSHAKE_ID	 0x5555
-#define ECG_INFO_ID      0xabcd
-
 
 status_t RMCALL_HOST_Tx(void *_data, uint32_t _dataSize);
 status_t RMCALL_HOST_Rx(void *_data, uint32_t _dataSize);
